e4e1fc432e07f5113446b8cba1733ac71240287b
artslob <artslob@yandex.ru>
2021-06-17 23:16:53 +0300

    add try_from [u8; 4] for chunk types

--- src/chunk_type.rs
+++ src/chunk_type.rs
@@ -1,3 +1,5 @@
+use std::convert::TryInto;
+
 enum AncillaryBit {
     Critical,  // UPPERCASE
     Ancillary, // lowercase
@@ -22,4 +24,53 @@ struct ChunkType {
     private_bit: PrivateBit,
     reserved_bit: ReservedBit,
     safe_to_copy_bit: SafeToCopyBit,
+    bytes: [u8; 4],
+    chars: [char; 4],
+    string: String,
+}
+
+impl ChunkType {
+    fn bytes(&self) -> [u8; 4] {
+        self.bytes
+    }
+}
+
+impl std::convert::TryFrom<[u8; 4]> for ChunkType {
+    type Error = String;
+
+    fn try_from(bytes: [u8; 4]) -> Result<Self, Self::Error> {
+        let ancillary_bit_char = char::from(bytes[0]);
+        let private_bit_char = char::from(bytes[1]);
+        let reserved_bit_char = char::from(bytes[2]);
+        let safe_to_copy_bit_char = char::from(bytes[3]);
+        let chars = [
+            ancillary_bit_char,
+            private_bit_char,
+            reserved_bit_char,
+            safe_to_copy_bit_char,
+        ];
+        Ok(ChunkType {
+            ancillary_bit: AncillaryBit::Critical,
+            private_bit: PrivateBit::Public,
+            reserved_bit: ReservedBit::Reserved,
+            safe_to_copy_bit: SafeToCopyBit::Unsafe,
+            bytes,
+            chars,
+            string: chars.iter().collect(),
+        })
+    }
+}
+
+mod tests {
+    use super::*;
+    use std::convert::TryFrom;
+    use std::str::FromStr;
+
+    #[test]
+    pub fn test_chunk_type_from_bytes() {
+        let expected = [82, 117, 83, 116];
+        let actual = ChunkType::try_from([82, 117, 83, 116]).unwrap();
+
+        assert_eq!(expected, actual.bytes());
+    }
 }
