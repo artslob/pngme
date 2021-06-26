4e1b4b1cb5d2fdda5449df38df3faad45952dadc
artslob <artslob@yandex.ru>
2021-06-26 22:39:37 +0300

    refactor main

--- src/main.rs
+++ src/main.rs
@@ -11,21 +11,11 @@ pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
 
 fn main() -> Result<()> {
-    let opts = crate::args::parse_cli();
-    // TODO merge match to 1 result?
-    match opts.sub_cmd {
-        SubCommand::Encode(cmd) => {
-            crate::commands::encode(cmd)?;
-        }
-        SubCommand::Decode(cmd) => {
-            crate::commands::decode(cmd)?;
-        }
-        SubCommand::Remove(cmd) => {
-            crate::commands::remove(cmd)?;
-        }
-        SubCommand::Print(cmd) => {
-            crate::commands::print(cmd)?;
-        }
+    match crate::args::parse_cli().sub_cmd {
+        SubCommand::Encode(cmd) => crate::commands::encode(cmd)?,
+        SubCommand::Decode(cmd) => crate::commands::decode(cmd)?,
+        SubCommand::Remove(cmd) => crate::commands::remove(cmd)?,
+        SubCommand::Print(cmd) => crate::commands::print(cmd)?,
     };
     Ok(())
 }
